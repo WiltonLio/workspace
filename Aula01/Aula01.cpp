f82857a5726c5786f73688dafd443df407bbbdf6
WiltonLio <wilton.lio@gmail.com>
2023-08-23 14:31:01 -0300

    inicial Branch

---
+++ Aula01/Aula01.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+
+using namespace std;
+
+int main (void){
+     
+     string nome;
+     int anoNascimento;
+     cout << "Digite o seu nome: ";
+     cin  >> nome;
+     cout << "Bom dia, " << nome << ". Tenha um bom dia.";
+     cout << endl;
+     cout << nome << ", você nasceu em que ano? ";
+     cin  >> anoNascimento;
+     cout << "Considerando que estamos em 2023, você tem "
+          << 2023-anoNascimento
+          <<" anos, ou está proximo de fazer."
+          << endl;
+    
+    return 0;
+
+
+}
